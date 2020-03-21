7caaccfc3764e659e4a5ed98a203bf061bc87e9b
hc-daewon <mackgear.hc@gmail.com>
2020-03-21 15:16:53 +0900

    joystick 연습 소스

---
+++ Assets/Scripts/JoyStick.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public bool isClick = false;
+    Vector2 origin, buttonDis;
+    RectTransform myTransform, paTransform;
+
+    public float Degree;
+
+    public GameObject TargetObject = null;
+    public string Message;
+
+    void Start()
+    {
+        myTransform = GetComponent<RectTransform>();
+        paTransform = transform.parent.GetComponent<RectTransform>();
+        origin = myTransform.position;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isClick)
+        {
+            buttonDis = eventData.position - origin;
+            isClick = true;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.position = origin;
+        isClick = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        myTransform.position = eventData.position - buttonDis;
+
+        Vector2 dir = (Vector2)myTransform.position - origin;
+        Degree = (Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + 360f) % 360f;
+
+        float dist = Vector3.Distance(myTransform.position, paTransform.position);
+        if (dist > 100f) myTransform.position = paTransform.position + (myTransform.position - paTransform.position).normalized * 100f;
+
+        if (TargetObject != null) TargetObject.SendMessage(Message, Degree);
+    }
+}
---
+++ Assets/Scripts/MyTank.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyTank : MonoBehaviour
+{
+    public JoyStick MyStick;
+    public GameButton Abutton, Bbutton, Cbutton;
+
+    ParticleSystem bullet;
+
+    public bool SlerpMode = true;
+
+
+	void Start()
+	{
+        bullet = GetComponentInChildren<ParticleSystem>();
+        bullet.enableEmission = false;
+	}
+	
+	void Update()
+	{
+	    if (MyStick.isClick)
+        {
+            transform.Translate(Vector3.forward * 4.0f * Time.deltaTime);
+
+            if (SlerpMode)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(MyStick.Degree, Vector3.up), Time.deltaTime * 2.0f); // 스틱 방향으로 서서히 회전
+            }
+            else
+            {
+                transform.rotation = Quaternion.AngleAxis(MyStick.Degree, Vector3.up); // 스틱 방향으로 바로 회전
+            }
+        }
+        bullet.enableEmission = Abutton.isClick;
+	}
+
+    void JumpTank()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 600f);
+    }
+}
